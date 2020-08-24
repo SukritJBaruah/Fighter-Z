3a7bc7cef0a19c7cca88aa9e7df4fb711b29c7c4
SukritJBaruah <68151213+SukritJBaruah@users.noreply.github.com>
2020-08-24 23:11:14 +0530

    enemy prefab

--- Assets/Scripts/Sukrit.cs
+++ Assets/Scripts/Sukrit.cs
@@ -187,7 +187,7 @@ public class Sukrit : MonoBehaviour
                                     rundirection = -1f;
                                 }
                             }
-                            print("running");
+                            //print("running");
                         }
                         else
                         {
